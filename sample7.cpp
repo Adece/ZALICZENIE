3778dea9de9a70a1100c895c68c23001a65530b4
Adece <mucoslaw2011@gmail.com>
2023-06-21 23:59:31 +0200

    Dodanie plikow sample 6 i sample 7

---
+++ sample7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	
+  int x, y;
+  int sum;
+  // Wypisanie liczby nr 1
+  cout << "Type a number: ";
+  cin >> x;
+   // Wypisanie liczby nr 2
+  cout << "Type another number: ";
+  cin >> y;
+  sum = x + y;
+  // Wypisanie wyniku
+  cout << "Sum is: " << sum;
+  return 0;
+}
