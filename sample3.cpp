dee7f070a08a54afd73751d19fbe8fe7c9f3bb2f
Adece <mucoslaw2011@gmail.com>
2023-06-21 20:11:38 +0200

    dodanie komentarzy w pliku sample3

--- sample3.cpp
+++ sample3.cpp
@@ -2,13 +2,18 @@
 using namespace std;
 
 int main() {
+	
+	
   int x, y;
   int sum;
+  // Wypisanie liczby nr 1
   cout << "Type a number: ";
   cin >> x;
+   // Wypisanie liczby nr 2
   cout << "Type another number: ";
   cin >> y;
   sum = x + y;
+  // Wypisanie wyniku
   cout << "Sum is: " << sum;
   return 0;
 }
